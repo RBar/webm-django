383e9c569214e94ca90167401e3a19b4c3ff919a
RBar <barraganroque5@gmail.com>
2020-05-13 02:32:47 -0500

    basic files

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return render (request, 'accounts/main.html')
+
+def products(request):
+    return render (request, 'accounts/products.html')
+
+def customer(request):
+    return render (request, 'accounts/customer.html')
+
+
